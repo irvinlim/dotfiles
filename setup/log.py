4cb85f1e74e0271010018e8ff59b77b39f9b7cfe
Irvin Lim <irvinlimweiquan@gmail.com>
2018-11-24 23:25:09 +0800

    Fix Python2.7 compatibility for setup scripts

--- setup/log.py
+++ setup/log.py
@@ -1,21 +1,26 @@
 from __future__ import print_function
 
 
-def success(*msg, sep=' ', **kwargs):
+def success(*msg, **kwargs):
+    sep = kwargs.pop('sep', ' ')
     print(color('0;32', *msg, sep=sep), **kwargs)
 
 
-def info(*msg, sep=' ', **kwargs):
+def info(*msg, **kwargs):
+    sep = kwargs.pop('sep', ' ')
     print(color('0;33', *msg, sep=sep), **kwargs)
 
 
-def error(*msg, sep=' ', **kwargs):
+def error(*msg, **kwargs):
+    sep = kwargs.pop('sep', ' ')
     print(color('0;31', *msg, sep=sep), **kwargs)
 
 
-def debug(*msg, sep=' ', **kwargs):
+def debug(*msg, **kwargs):
+    sep = kwargs.pop('sep', ' ')
     print(color('0;36', *msg, sep=sep), **kwargs)
 
 
-def color(colorcode, *msg, sep=' '):
+def color(colorcode, *msg, **kwargs):
+    sep = kwargs.pop('sep', ' ')
     return '\033[%sm' % colorcode + sep.join(msg) + '\033[0m'
